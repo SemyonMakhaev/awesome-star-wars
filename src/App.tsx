76e96225eac8ec1da65ea704510ffb1f1a01be2d
Semyon Makhaev <semenmakhaev@yandex.ru>
2023-08-03 17:33:28 +0300

    Add metrika

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { Provider as StoreProvider } from 'react-redux';
+import { YMInitializer } from 'react-yandex-metrika';
 import { store } from 'src/store/root';
 import { MainLayout } from 'src/layouts/MainLayout';
 import { ErrorBoundary } from 'src/components/ErrorBoundary';
@@ -15,6 +16,15 @@ function App() {
           <RouterProvider router={router} />
         </MainLayout>
       </StoreProvider>
+      <YMInitializer
+        accounts={[94526988]}
+        options={{
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+          webvisor: true,
+        }}
+      />
     </ErrorBoundary>
   );
 }
